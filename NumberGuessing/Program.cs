c7e84b66505a7d6a86981e3e8d80de11dbb35e82
Aphar Magaramova <77408835+ApharMagaramova@users.noreply.github.com>
2022-02-19 16:20:34 +0400

    Add files via upload

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int WinNumber = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess the number.");
+
+                int number = Int32.Parse(Console.ReadLine());
+
+                if (number < WinNumber)
+                {
+                    Console.WriteLine("To Low. Guess higher...");
+                }
+
+                else if (number > WinNumber)
+                {
+                    Console.WriteLine("To high. Guess lower..");
+                }
+
+                else if (number == WinNumber)
+                {
+                    Console.WriteLine("You Win!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing guessing game!");
+            Console.WriteLine("Press twice any key to finish");
+            Console.ReadKey(true);
+        }
+    }
+}
